f619cdf0f4fbbf7db5b16861d5a3a9364394b22b
xuzhao <xuzhao@zhique.design>
2022-09-27 21:14:28 +0800

    [DELETE] 删除value

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -3,10 +3,10 @@ import ReactDom from 'react-dom';
 
 import { MarkdownEditor } from '../../src';
 
+import 'github-markdown-css/github-markdown-light.css';
+
 import 'codemirror/lib/codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/gfm/gfm';
 
-import 'github-markdown-css/github-markdown.css';
-
-ReactDom.render(<MarkdownEditor value="这是一个demo" cmOptions={{ mode: 'gfm' }} />, document.getElementById('root'));
+ReactDom.render(<MarkdownEditor cmOptions={{ mode: 'gfm' }} />, document.getElementById('root'));
